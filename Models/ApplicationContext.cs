6b75487bbbb25f63bd9a5c8a36a1ee7cc4a7e4f7
r4p1er <tsyganok2015@gmail.com>
2020-09-21 19:36:45 +0300

    Added profile model

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -9,6 +9,7 @@ namespace Dacha.Models
     public class ApplicationContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
     }
 }
---
+++ Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dacha.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public int Place { get; set; }
+    }
+}
