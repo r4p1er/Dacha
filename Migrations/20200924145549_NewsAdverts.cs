e617d9aac4b8131db7712a4e826244d44bce3183
r4p1er <tsyganok2015@gmail.com>
2020-09-24 17:56:30 +0300

    Added migrations

---
+++ Migrations/20200924145549_NewsAdverts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dacha.Migrations
+{
+    public partial class NewsAdverts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adverts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Body = table.Column<string>(maxLength: 1500, nullable: false),
+                    Contact = table.Column<string>(maxLength: 1500, nullable: false),
+                    ExpDate = table.Column<DateTime>(nullable: false),
+                    ProfileId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adverts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adverts_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Body = table.Column<string>(maxLength: 1500, nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_ProfileId",
+                table: "Adverts",
+                column: "ProfileId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adverts");
+
+            migrationBuilder.DropTable(
+                name: "News");
+        }
+    }
+}
