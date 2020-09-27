796668ab0ed50c73b60e60020da8adf6480f36c7
m1xa4kala <maikl2020011@gmail.com>
2020-09-27 21:01:54 +0300

    refactor return docs

--- Frontend/src/components/Documents/Documents.jsx
+++ Frontend/src/components/Documents/Documents.jsx
@@ -1,25 +1,33 @@
-import Axios from "axios";
+import axios from "axios";
 import React, { Component } from "react";
 import { Button, Container, Form, FormControl } from "react-bootstrap";
 class Documents extends Component {
-  state = {
-    selectedFile: {},
-  };
+  constructor(props) {
+    super(props);
+    this.state = {selectedFile: null}
+  }
 
   onFileChange = (event) => {
-    this.setState({ selectedFile: event.target.files[0] });
+    const file = event.target.files[0];
+    this.setState({ selectedFile: file });
   };
 
-  onFileUpload = () => Axios.post("http://localhost:5000/api/documents", this.state.selectedFile);
-  
+  onFileUpload = () => {
+    const data = new FormData()
+    data.append(`${this.state.selectedFile.name}`, this.state.selectedFile)
+    axios.post("http://localhost:5000/api/documents", data);
+  };
+
   render() {
     return (
       <>
         <Container>
           <h1>Documents Page</h1>
-          <Form>
+          <Form enctype="multipart/form-data" method="post" onSubmit={this.onFileUpload}>
             <FormControl type="file" onChange={this.onFileChange} />
-              <Button variant="primary" onClick={this.onFileUpload}>Загрузить документ</Button>
+            <Button variant="primary" type="submit">
+              Загрузить документ
+            </Button>
           </Form>
         </Container>
       </>
