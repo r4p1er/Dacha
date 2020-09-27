d1abad5bde1a129db35d89e116e57b9a0e3ed31e
m1xa4kala <maikl2020011@gmail.com>
2020-09-27 21:17:43 +0300

    refactor uploadFile to async

--- Frontend/src/components/Documents/Documents.jsx
+++ Frontend/src/components/Documents/Documents.jsx
@@ -1,38 +1,43 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Container, Form, FormControl } from "react-bootstrap";
-class Documents extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {selectedFile: null}
-  }
 
-  onFileChange = (event) => {
-    const file = event.target.files[0];
-    this.setState({ selectedFile: file });
+const Documents = () => {
+
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState();
+
+  const saveFile = (event) => {
+    setFile(event.target.files[0]);
+    setFileName(event.target.files[0].name);
   };
 
-  onFileUpload = () => {
-    const data = new FormData()
-    data.append(`${this.state.selectedFile.name}`, this.state.selectedFile)
-    axios.post("http://localhost:5000/api/documents", data);
+  const uploadFile = async (event) => {
+    const formData = new FormData();
+    formData.append('formFile', file);
+    formData.append('fileName', fileName);
+    try {
+      const res = await axios.post("http://localhost:5000/api/documents", formData);
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
   };
-
-  render() {
+    
+  
     return (
       <>
         <Container>
           <h1>Documents Page</h1>
-          <Form enctype="multipart/form-data" method="post" onSubmit={this.onFileUpload}>
-            <FormControl type="file" onChange={this.onFileChange} />
-            <Button variant="primary" type="submit">
+          <Form enctype="multipart/form-data">
+            <FormControl type="file" onChange={saveFile} />
+            <Button variant="primary" onClick={uploadFile}>
               Загрузить документ
             </Button>
           </Form>
         </Container>
       </>
     );
-  }
 }
 
 export default Documents;
