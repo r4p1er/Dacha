bc21748a905a5aaca7e2fb5191843ea97e92138d
m1xa4kala <maikl2020011@gmail.com>
2020-09-28 03:33:16 +0300

    added Admins page and actions for adverts

--- Frontend/src/redux/actions/adverts.js
+++ Frontend/src/redux/actions/adverts.js
@@ -1,17 +1,30 @@
 import axios from "axios";
-import { setAdverts, addAdvert } from "../reducers/adverts";
+import { setAd, addAd, deleteAd, changeAd } from "../reducers/adverts";
 
 export const getAdverts = () => {
   return async (dispatch) => {
     const adverts = await axios.get("http://localhost:5000/api/adverts")
-    dispatch(setAdverts(adverts.data))
+    dispatch(setAd(adverts.data))
   }
 }
-
 
 export const createAdvert = (advert) => {
   return async (dispatch) => {
     await axios.post("http://localhost:5000/api/adverts", advert)
-    dispatch(addAdvert(advert))
+    dispatch(addAd(advert))
+  }
+}
+
+export const changeAdvert = (advert) => {
+  return async (dispatch) => {
+    await axios.put(`http://localhost:5000/api/adverts${advert.id}`, advert)
+    dispatch(changeAd(advert))
+  }
+}
+
+export const deleteAdvert = (id) => {
+  return async (dispatch) => {
+    await axios.delete(`http://localhost:5000/api/adverts${id}`)
+    dispatch(deleteAd(id))
   }
 }
