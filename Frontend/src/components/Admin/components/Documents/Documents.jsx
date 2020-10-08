a147190f5317502081d3f550e9c1f2738ed183ef
m1xa4kala <maikl2020011@gmail.com>
2020-10-08 19:44:47 +0300

    added document management for administrators

--- Frontend/src/components/Admin/components/Documents/Documents.jsx
+++ Frontend/src/components/Admin/components/Documents/Documents.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import axios from "axios";
+import fileDownload from "js-file-download";
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form, FormControl } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { getDocuments } from "../../../../redux/actions/documents";
 
 const Documents = () => {
-  return <h1>Documents</h1>;
+  const dispatch = useDispatch();
+  const documents = useSelector((state) => state.docs.items);
+  useEffect(() => {
+    dispatch(getDocuments());
+  }, []);
+
+  const token = localStorage.jwtToken;
+  const AuthStr = "Bearer ".concat(token);
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState();
+
+  const saveFile = (event) => {
+    setFile(event.target.files[0]);
+    setFileName(event.target.files[0].name);
+  };
+
+  const uploadFile = async (event) => {
+    const formData = new FormData();
+    formData.append("formFile", file);
+    formData.append("fileName", fileName);
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/documents",
+        formData
+      );
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  return (
+    <>
+      <h1>Документы</h1>
+      <Container>
+        <Form encType="multipart/form-data">
+          <FormControl type="file" onChange={saveFile} />
+          <Button variant="primary" onClick={uploadFile}>
+            Загрузить документ
+          </Button>
+        </Form>
+        {documents.map((doc) => (
+          <Button
+            key={doc.id}
+            variant="primary"
+            onClick={() => {
+              axios
+                .get(`http://localhost:5000/api/documents/${doc.id}`, {
+                  responseType: "blob",
+                  headers: { Authorization: AuthStr },
+                })
+                .then((response) => {
+                  fileDownload(response.data, `${doc.name}`);
+                });
+            }}
+          >
+            {doc.name}
+          </Button>
+        ))}
+      </Container>
+    </>
+  );
 };
 
 export default Documents;
--- Frontend/src/components/Documents/Documents.jsx
+++ Frontend/src/components/Documents/Documents.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDocuments } from "../../redux/actions/documents";
 import fileDownload from "js-file-download";
-import { Button, Container, Form, FormControl } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 
 const Documents = () => {
   const dispatch = useDispatch();
@@ -14,38 +14,11 @@ const Documents = () => {
 
   const token = localStorage.jwtToken;
   const AuthStr = "Bearer ".concat(token);
-  const [file, setFile] = useState();
-  const [fileName, setFileName] = useState();
-
-  const saveFile = (event) => {
-    setFile(event.target.files[0]);
-    setFileName(event.target.files[0].name);
-  };
-
-  const uploadFile = async (event) => {
-    const formData = new FormData();
-    formData.append("formFile", file);
-    formData.append("fileName", fileName);
-    try {
-      const res = await axios.post(
-        "http://localhost:5000/api/documents",
-        formData
-      );
-      console.log(res);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+ 
   return (
     <>
       <Container>
-        <h1>Documents Page</h1>
-        <Form encType="multipart/form-data">
-          <FormControl type="file" onChange={saveFile} />
-          <Button variant="primary" onClick={uploadFile}>
-            Загрузить документ
-          </Button>
-        </Form>
+        <h1>Документы</h1>
         {documents.map((doc) => (
           <Button
             key={doc.id}
