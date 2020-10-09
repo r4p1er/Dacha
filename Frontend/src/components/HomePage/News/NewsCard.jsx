478d1ddcff223d9ba2dea81a6de03dc3748e4336
m1xa4kala <maikl2020011@gmail.com>
2020-10-09 20:11:41 +0300

    added href parse func for news

--- Frontend/src/components/HomePage/News/NewsCard.jsx
+++ Frontend/src/components/HomePage/News/NewsCard.jsx
@@ -3,10 +3,25 @@ import React from "react";
 const NewsCard = ({ title, body, date }) => {
   const newsDate = new Date(date).toLocaleString();
 
+  function urlify(text) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.split(urlRegex)
+       .map(part => {
+          if(part.match(urlRegex)) {
+             return <a href={part} rel="noopener noreferrer" target="_blank">{part}</a>;
+          }
+          return part;
+       });
+  }
+
+  const headingAvailable = (
+    <span className="home_post_text">{urlify(body)}</span>
+  );
+
   return (
     <div>
       <h5>{title}</h5>
-      <p>{body}</p>
+      <p>{headingAvailable}</p>
       <span>{newsDate}</span>
     </div>
   );
