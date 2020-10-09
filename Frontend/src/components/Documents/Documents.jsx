feeaa2a05981621033a8daa67a9c279869aa77b9
m1xa4kala <maikl2020011@gmail.com>
2020-10-09 22:09:19 +0300

    moved extension read function to utilities

--- Frontend/src/components/Documents/Documents.jsx
+++ Frontend/src/components/Documents/Documents.jsx
@@ -3,13 +3,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDocuments } from "../../redux/actions/documents";
 import fileDownload from "js-file-download";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
-import icon_doc from "../../additions/icon_doc.png";
-import icon_jpg from "../../additions/icon_jpg.png";
-import icon_pdf from "../../additions/icon_pdf.png";
-import icon_png from "../../additions/icon_png.png";
-import icon_txt from "../../additions/icon_txt.png";
-import icon_xls from "../../additions/icon_xls.png";
+import fileExtentionRead from "../../utils/fileExtentionReader";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 const Documents = () => {
   const dispatch = useDispatch();
@@ -20,39 +15,6 @@ const Documents = () => {
 
   const token = localStorage.jwtToken;
   const AuthStr = "Bearer ".concat(token);
-
-  const fileExtentionRead = (name) => {
-    const allowedExtention = name.match(
-      /(\.doc|\.docx|\.jpg|\.jpeg|\.png|\.xls|\.xlsx|\.pdf|\.txt)$/i
-    );
-    let fileExtention = 1;
-    !allowedExtention
-      ? (fileExtention = 1)
-      : (fileExtention = allowedExtention[0]);
-
-    switch (fileExtention) {
-      case ".doc":
-        return icon_doc;
-      case ".docx":
-        return icon_doc;
-      case ".jpg":
-        return icon_jpg;
-      case ".jpeg":
-        return icon_jpg;
-      case ".png":
-        return icon_png;
-      case ".xls":
-        return icon_xls;
-      case ".xlsx":
-        return icon_xls;
-      case ".pdf":
-        return icon_pdf;
-      case ".txt":
-        return icon_txt;
-      default:
-        return icon_doc;
-    }
-  };
 
   return (
     <>
