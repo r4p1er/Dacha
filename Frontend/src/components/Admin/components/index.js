c5618a7bbea9d8f2f005783fab982d9a433b5ae7
m1xa4kala <maikl2020011@gmail.com>
2020-10-14 04:25:55 +0300

    added lazy load admin pages

--- Frontend/src/components/Admin/components/index.js
+++
@@ -1,7 +0,0 @@
-import AdminNews from "./News/News";
-import AdminDocs from "./Documents/Documents";
-import AdminAds from "./Adverts/Adverts";
-import AdminVote from "./Vote/Vote";
-import AdminAccounts from "./Accounts/Accounts";
-
-export {AdminNews, AdminDocs, AdminAds, AdminVote, AdminAccounts};
--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,6 +1,5 @@
 import Login from "./Login/LoginForm";
 import NotFound from "./NotFound/NotFound";
-import Admin from "./Admin/AdminContainer";
 import Header from "./Header/Header";
 import Home from "./HomePage/HomeContainer";
 import AdvertsContainer from "./Adverts/AdvertsContainer";
@@ -8,7 +7,5 @@ import Adverts from "./Adverts/Adverts";
 import CurrentAdverts from "./Adverts/CurrentAdverts";
 import Documents from "./Documents/Documents";
 import Vote from "./Vote/Vote";
-import FullPageLoader from "./Loader/Loader";
 
-
-export {Header, Home, Adverts, Documents, Vote, Login, NotFound, Admin, FullPageLoader, CurrentAdverts, AdvertsContainer};
+export {Header, Home, Adverts, Documents, Vote, Login, NotFound, CurrentAdverts, AdvertsContainer};
