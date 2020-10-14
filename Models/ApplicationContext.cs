f3ef345160d51080924874898980f05416dbb4e3
r4p1er <tsyganok2015@gmail.com>
2020-10-14 20:04:51 +0300

    Changed admins password and place

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -29,11 +29,11 @@ namespace Dacha.Models
             {
                 Id = 1,
                 Login = "admin",
-                Password = "admin",
+                Password = "semiconductor",
                 Name = "Rapier",
                 MiddleName = "M1xa4kala",
                 LastName = "Neef4",
-                Place = 228,
+                Place = 0,
                 RoleId = 3
             });
         }
