302092fc74fa1f7ef490142aef9f721c404a3661
m1xa4kala <maikl2020011@gmail.com>
2020-10-28 11:18:47 +0300

    fixed header expand size

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ function Header(props) {
         inline="true"
         collapseOnSelect="true"
         expanded={expanded}
-        expand="lg"
+        expand="xl"
       >
         <Link
           onClick={() => setExpanded(false)}
